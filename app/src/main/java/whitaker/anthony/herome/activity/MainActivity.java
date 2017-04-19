2d12e781a175b5dd91e9c836f36dd12c3aee3da9
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-04-19 12:31:05 -0400

    Initial commit. Add drawables. Create fragment.

---
+++ app/src/main/java/whitaker/anthony/herome/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package whitaker.anthony.herome.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import whitaker.anthony.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
